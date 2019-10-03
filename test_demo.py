1f412ef4f2ea71e04a7c06aaefaf4e0b1c7a1e5e
garzuzo <garzuxo@gmail.com>
2019-10-02 19:20:48 -0500

    add sub test

--- test_demo.py
+++ test_demo.py
@@ -5,6 +5,11 @@ def test_add():
 # GIVEN two numbers
 # WHEN I invoke the method
 # THEN I get the addition of two numbers
-    add_result = demo.add(3,4)
     assert demo.add(2,4)==6
+
+def test_sub():
+# GIVEN two numbers
+# WHEN I invoke the method
+# THEN I get the addition of two numbers
+    assert demo.sub(2,4)==-2
 
